3779d199b859522ff170de89728a9aa8d3f82e11
Anish <anishpandey021@gmail.com>
2026-02-04 23:20:38 -0600

    added test notebook to test data structure and cleanup

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ data= pd.read_csv('./data/customer_support_tickets.csv')
 # print(data.columns) 
 # print(data.iloc[0])
 print(data["Ticket Type"].unique())
-# todo clean data for ticket type to be one-word-strings
- 
+# todo clean data for ticket type to be one-word-strins
+
 
