b3608c21ccec931d0c021b36cc91c54273f234e0
Anish <anishpandey021@gmail.com>
2026-02-12 23:51:56 -0600

    vectorizationn of text features using TFIDF

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+
 
 data= pd.read_csv('./data/customer_support_tickets.csv')
 
@@ -60,3 +64,9 @@ x= data["text"]
 y= data["label"]
 
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42, stratify=y)
+
+vectorizer=TfidfVectorizer(ngram_range=(1, 2), min_df=5, max_df=0.9, stop_words="English", sublinear_tf=True)
+x_train_vec=vectorizer.fit_transform(X_train)
+x_test_vec=vectorizer.fit(X_test)
+
+
