a2ff4b1e64e78bd18670d7b43c81c3e473d8ca19
Anish <anishpandey021@gmail.com>
2026-02-24 18:45:48 -0600

    fix: changed pathname to support render deployment

--- training/train.py
+++ training/train.py
@@ -4,6 +4,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from datasets import load_dataset
 import json
+import os
 
 dataset = load_dataset("banking77")
 
